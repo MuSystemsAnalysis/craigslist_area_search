c84c686fa52b7458574e5f52fef6a12e71d81fcb
Ubuntu <ubuntu@ip-172-31-45-161.ec2.internal>
2015-09-30 18:13:25 +0000

    Remove web requests; get html from local .htm
    Apparently craigslist didn't appreciate my bombarding their page
    with requests while debugging the scraper, and honestly, there's
    no reason I need to grab a fresh copy of the site every time
    the script will run.
    
    Were this an important enterprise application (to scrape
    craigslist for US cities, good lord) then confirming that they'd
    not added any more cities/removed anything would be prudent.
    
    Local copy should be in same directory, named "sites.htm"

--- areaScraper.py
+++ areaScraper.py
@@ -3,13 +3,15 @@
 # For sp2015 Systems Analysis and Design
 
 from bs4 import BeautifulSoup
-import requests
 import re
 
-soup = BeautifulSoup(requests.get("http://www.craigslist.org/about/sites").text, "html.parser")
+fh = open("sites.htm", "r")
+soup = BeautifulSoup(fh, "html.parser")
 
 for columnDiv in soup.h1.next_sibling.next_sibling:
 	for state in columnDiv:
 		for city in state:
 			print(city)
+
+#print(soup.text)
 print("\n----Done----\n\n")
