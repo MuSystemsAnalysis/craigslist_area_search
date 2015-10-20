ab9ab1f1bf1b0b05053d3e2908af4c660fc6041d
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-10-20 13:23:52 +0000

    Add saveToDatabase module (by Chris D)

---
+++ saveToDatabase.py
@@ -0,0 +1,35 @@
+import sqlite3
+import sys
+
+database_file = 'test.db'
+cityList = ['Dallas', 'Washington', 'Philadelphia'];
+
+#connect
+conn = sqlite3.connect(database_file)
+cursor = conn.cursor()
+
+
+#create table
+cursor.execute('DROP TABLE IF EXISTS cities')
+cursor.execute('''CREATE TABLE cities
+    (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    NAME TEXT NOT NULL);''')
+print("Table created...\n")
+
+
+#insert
+for city in cityList:
+    format_str = """INSERT INTO cities (ID, NAME) VALUES (NULL, "{cityName}");"""
+    sql_command = format_str.format(cityName=city)
+    cursor.execute(sql_command)
+
+#view
+c = conn.execute("SELECT * FROM cities")
+for row in c:
+    print ("ID = ", row[0])
+    print ("NAME = ", row[1])
+
+
+#save and close the database
+conn.commit()
+conn.close()
